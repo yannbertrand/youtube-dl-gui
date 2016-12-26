36d3cb1848812996cc08377c176645cbb73e9b28
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2016-12-26 15:49:36 +0100

    Rotate the + button on form submit

--- src/form/form.js
+++ src/form/form.js
@@ -2,8 +2,10 @@ export var init = function () {
     const $form = $('form');
     const $input = $form.find('input');
     const $submit = $form.find('button');
+    const $icon = $submit.find('span');
 
     $form.on('submit', function (event) {
+        $icon.addClass('fa-spin');
         $submit.prop('disabled', true);
         event.preventDefault();
     });
