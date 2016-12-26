46522a29c44cee8fdf5dd428a7240af241d11974
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2016-12-26 20:01:54 +0100

    Add a new row to table when submiting form

--- src/form/form.js
+++ src/form/form.js
@@ -1,12 +1,58 @@
+const videos = [];
+
 export var init = function () {
     const $form = $('form');
+    const $table = $('table');
     const $input = $form.find('input');
     const $submit = $form.find('button');
     const $icon = $submit.find('span');
 
+    $table.hide();
+
     $form.on('submit', function (event) {
         $icon.addClass('fa-spin');
         $submit.prop('disabled', true);
+
+        downloadVideoAndAddRowToTable($input.val(), function (error) {
+            if (!error) {
+                $input.val('').focus();
+            }
+
+            $icon.removeClass('fa-spin');
+            $submit.prop('disabled', false);
+        });
+
         event.preventDefault();
     });
 };
+
+function downloadVideoAndAddRowToTable(link, callback) {
+    const $table = $('table');
+    const video = getVideoInfo(link);
+    const $tr = $(videoToHTML(video));
+
+    $table.append($tr);
+    $table.show();
+
+    callback();
+}
+
+function videoToHTML(video) {
+    return '<tr>' +
+            '<td>' + video.name + '</td>' +
+            '<td>' + video.channel + '</td>' +
+            '<td>' + video.duration + '</td>' +
+            '<td>' + video.size + '</td>' +
+            '<td>' + video.status + '</td>' +
+        '</tr>';
+}
+
+function getVideoInfo(link) {
+    return {
+        name: 'Hello',
+        channel: 'World',
+        duration: '00:00',
+        size: '10MB',
+        status: 'Downloading...',
+    };
+}
