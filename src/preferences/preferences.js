73ca1743af5deaab7f6538a2aa0bd33bc5b84301
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2017-01-21 15:46:14 +0100

    Really change base destination

--- src/preferences/preferences.js
+++ src/preferences/preferences.js
@@ -1,15 +1,27 @@
-import { getBaseDestination } from '../storage/storage';
+import { getBaseDestination, setBaseDestination } from '../storage/storage';
 
-const path = require('path');
-const fs = require('fs');
 const { remote } = require('electron');
 
-let baseDestination;
-let filePath;
 
 export var init = function () {
   console.log('init preferences');
-  baseDestination = getBaseDestination();
 
-  $('input#base-destination').val(baseDestination);
+  refreshBaseDestination();
+  $('input#base-destination').parent().find('button').on('click', selectBaseDestination);
 };
+
+function selectBaseDestination() {
+  remote.dialog.showOpenDialog(remote.BrowserWindow.getFocusedWindow(), {
+    defaultPath: getBaseDestination(),
+    properties: ['openDirectory'],
+  }, function (filePaths) {
+    if (filePaths === undefined) return;
+
+    setBaseDestination(filePaths[0]);
+    refreshBaseDestination();
+  });
+}
+
+function refreshBaseDestination() {
+  $('input#base-destination').val(getBaseDestination());
+}
