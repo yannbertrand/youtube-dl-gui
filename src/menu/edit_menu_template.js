baf2a1e91d77cccbda8da20942fb7c7ec228ac69
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2017-01-21 14:55:26 +0100

    Add a preferences menu item which open the new browser window

--- src/menu/edit_menu_template.js
+++ src/menu/edit_menu_template.js
@@ -1,6 +1,15 @@
+import createWindow from '../helpers/window';
+import env from '../env';
+import path from 'path';
+import url from 'url';
+
 export var editMenuTemplate = {
     label: 'Edit',
     submenu: [
+        { role: 'about' },
+        { type: "separator" },
+        { label: "Preferences...", selector: "preferences:", click: openPreferencesWindow },
+        { type: "separator" },
         { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
         { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
         { type: "separator" },
@@ -10,3 +19,20 @@ export var editMenuTemplate = {
         { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
     ]
 };
+
+function openPreferencesWindow() {
+    var preferencesWindow = createWindow('preferences', {
+        width: 500,
+        height: 400
+    });
+
+    preferencesWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'preferences.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    if (env.name === 'development') {
+        preferencesWindow.openDevTools();
+    }
+}
