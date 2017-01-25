5e75c58181b111665c9936aecfb3751c9a679a7d
Yann Bertrand <yann.bertrand@orange.com>
2017-01-25 13:57:01 +0100

    Add downloads storage methods

--- src/storage/storage.js
+++ src/storage/storage.js
@@ -6,6 +6,7 @@ const DEFAULT_BASE_DESTINATION = path.join(remote.app.getPath('videos'), 'YouTub
 
 const KEYS = {
   BASE_DESTINATION: 'base_destination',
+  DOWNLOADS: 'downloads',
 };
 
 let config;
@@ -14,6 +15,10 @@ export var init = function () {
 
   if (! config.has(KEYS.BASE_DESTINATION)) {
     config.set(KEYS.BASE_DESTINATION, DEFAULT_BASE_DESTINATION);
+  }
+
+  if (! config.has(KEYS.DOWNLOADS)) {
+    config.set(KEYS.DOWNLOADS, {});
   }
 };
 
@@ -35,4 +40,28 @@ export var getBaseDestination = function () {
 
 export var setBaseDestination = function (baseDestination) {
   config.set(KEYS.BASE_DESTINATION, baseDestination);
+}
+
+// Downloads
+export var getDownloads = function () {
+  return config.get(KEYS.DOWNLOADS);
+}
+
+export var hasVideoInDownloads = function (id) {
+  return Object.keys(getDownloads()).indexOf(id) > -1;
+}
+
+export var addVideoInDownloads = function (id, info) {
+  const downloads = config.get(KEYS.DOWNLOADS);
+  if (hasVideoInDownloads(id)) {
+    throw new Error('Already in list');
+  }
+
+  downloads[id] = info;
+
+  config.set(KEYS.DOWNLOADS, downloads);
+}
+
+export var removeVideoFromDownloads = function (id) {
+  config.delete(KEYS.DOWNLOADS + '.' + id);
 }
